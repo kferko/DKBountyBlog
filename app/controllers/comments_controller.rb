6e45662d6865170a9c32e535013e2c56badcc00e
Kevin Ferko <kevinferko@gmail.com>
2016-01-08 10:58:14 -0500

    Comment fields in

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,28 @@
 class CommentsController < ApplicationController
   def index
+    @comment = Comments.all
   end
 
   def show
+    @comment = Comment.find(params[:id])
   end
 
   def new
+    @comment = Comment.new
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = session[:user_id]
+    if @comment.save
+      # @user.comments.push @comment
+      flash[:notice] = "Awesome New Comment"
+    else
+      flash[:alert] = "Please enter a comment"
+      redirect_to :back
+    end
   end
+
 
   def edit
   end
@@ -18,5 +31,12 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+  @comment.destroy
+  redirect_to :back
   end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:title, :body)
 end
