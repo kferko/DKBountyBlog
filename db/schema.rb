a3ba19aa6abcf523ae6a08587f5839bf7c923b60
Kevin Ferko <kevinferko@gmail.com>
2016-01-09 22:04:05 -0500

    one more time

--- db/schema.rb
+++ db/schema.rb
@@ -26,12 +26,8 @@ ActiveRecord::Schema.define(version: 20160110004707) do
     t.string   "title"
     t.string   "body"
     t.integer  "user_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "image"
   end
 
