3ac6c0e919025e07ff2590a197ecfe4af87e30f3
Dylan Weinberger <djweinbe@gmail.com>
2016-01-11 17:45:52 -0500

    fixed merge conflicts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,96 @@
 //= require turbolinks
 //= require_tree .
 
+// window.onload = function () {
+//     if (localStorage.get("hasCodeRunBefore") === null) {
+//             $(function() {
+//                   $(".lightup").delay(1000).fadeIn(300);
+//                   // $(".postcontainer,.commentcontainer,.userin,.userlist").fadeOut(1000);
+//                 });
+//         localStorage.setItem("hasCodeRunBefore", true);
+//     }
+// }
+
+window.onload = function () {
+    $(".lightup").delay(1000).fadeIn(300);
+    $(".hovertop").delay(1600).fadeIn(500);
+    $(".hoverbottom").delay(1600).fadeIn(500);
+    $(".optics").delay(3200).fadeIn(500);
+    // $(".cm1").delay(5300).addClass("ot1-anim");
+    setTimeout(function(){
+      $(".cm1").toggleClass("ot1-anim");
+      $(".cm2").toggleClass("ot2-anim");
+      $(".cm3").toggleClass("ot3-anim");
+      $(".cm4").toggleClass("ot4-anim");
+     }, 4000);
+    $(".postcontainer").delay(6200).fadeIn(1000);
+    $(".commentaddition").delay(6600).fadeIn(500);
+    $(".commentcontainer").delay(6900).fadeIn(500);
+    $(".userdisplaycontainer").delay(6200).fadeIn(1000);
+}
+
+
+// function toBeExecutedOnFirstLoad(){
+//   $(function() {
+//     $(".lightup").delay(1000).fadeIn(300);
+//     $(".hovertop").delay(1100).fadeIn(300);
+//     $(".hoverbottom").delay(1100).fadeIn(300);
+//     // $(".postcontainer,.commentcontainer,.userin,.userlist").fadeOut(1000);
+//   });
+// }
+// if(localStorage.getItem('first') === null){
+//   toBeExecutedOnFirstLoad();
+//   localStorage.setItem('first','nope!');
+// }
+
+// $('#linkc a').click(function (e) {
+//     e.preventDefault();                   // prevent default anchor behavior
+//     var goTo = this.getAttribute("href"); // store anchor href
+//     $(".postcontainer").delay(2000).fadeOut(500);
+//     $(".commentaddition").delay(2000).fadeOut(500);
+//     $(".commentcontainer").delay(2000).fadeOut(500);
+//     $(".userdisplaycontainer").delay(2000).fadeOut(500);
+//     // setTimeout(function(){
+//     //   $(".cm1").toggleClass("ot5-anim");
+//     //   $(".cm2").toggleClass("ot6-anim");
+//     //   $(".cm3").toggleClass("ot7-anim");
+//     //   $(".cm4").toggleClass("ot8-anim");
+//     //  }, 1200);
+//       $(".optics").delay(1500).fadeOut(500);
+//       $(".hovertop").delay(1800).fadeOut(500);
+//       $(".hoverbottom").delay(1800).fadeOut(500);
+//       $(".lightup").delay(2000).fadeOut(500);
+//     setTimeout(function(){
+//          window.location = goTo;
+//     },5000);       
+// }); 
+
+
+$('a').click(function(e) {
+  e.preventDefault();
+  var destination = $(this).attr('href');
+      $(".postcontainer").delay(2000).fadeOut(500);
+      $(".commentaddition").delay(2000).fadeOut(500);
+      $(".commentcontainer").delay(2000).fadeOut(500);
+  setTimeout(function() { window.location.href = destination; }, 6000);
+});
+
+// $('a').click(function (e) { 
+//     e.preventDefault();
+//     var goTo = this.getAttribute("href");
+
+//       $(".postcontainer").fadeOut(1000);
+//       $(".commentaddition").fadeOut(1000);
+//       $(".commentcontainer").fadeOut(1000);
+  
+//     setTimeout(function(){
+//          window.location = goTo;
+//     },3000);
+   
+// });
+
+
+
 var y = 0;
 $(function() {
   $("#clickmb1").click(function() {
@@ -87,7 +177,3 @@ $(function() {
 });
 });
 });
-// $("#img1").on('click', function() {
-//    $("#div1").fadeIn();
-//    $("#div2,#div3,#div4").fadeOut();
-// });
